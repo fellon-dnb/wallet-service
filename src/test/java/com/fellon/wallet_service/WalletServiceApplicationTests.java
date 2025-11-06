e9a9d8d62cf27a715997d31d19f5b64232e331ec
fellon-dnb <fellon@ancevt.com>
2025-11-06 11:51:10 +0300

    feat: initial Spring Boot project setup

---
+++ src/test/java/com/fellon/wallet_service/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fellon.wallet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
